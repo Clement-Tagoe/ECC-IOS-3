b03cced9a2cfab61884b7d619428b800d86ceb13
Clement-Tagoe <clementtagoe110@gmail.com>
2026-05-25 15:33:23 +0000

    24th commit

--- app/Livewire/FilesManager.php
+++ app/Livewire/FilesManager.php
@@ -3,16 +3,20 @@
 namespace App\Livewire;
 
 use App\Models\File;
+use App\Models\User;
+use Closure;
 use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 use Filament\Forms;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 use Livewire\WithFileUploads;
-use Closure;
 use Livewire\WithPagination;
 
 
@@ -25,12 +29,47 @@ class FilesManager extends Component implements HasActions, HasForms
     public ?File $parent = null;
     public ?File $currentFolder;
     public $folder = '';
+    public string $currentPath = '';
+
 
     public function getRoot()
     {
         return File::query()->whereIsRoot()->where('created_by', Auth::user()->id)->firstOrFail();
     }
     
+    protected function parentRules(?int $user_id = null):array
+    {
+        return [
+            Rule::exists(File::class, 'id')
+                ->where(function (Builder $query) use ($user_id) {
+                    return $query
+                        ->where('is_folder', '=', '1')
+                        ->where('created_by', '=' , $user_id);
+                })
+        ];
+    }
+
+    protected function saveFile($file, $user, $parent): void
+    {
+        $path = Storage::disk('public')->move(
+            $file,
+            'files/' . basename($file)
+        );
+
+        $model = new File();
+        $model->storage_path = $path;
+        $model->is_folder = false;
+        $model->name = $file->getClientOriginalName();
+        $model->mime = $file->getMimeType();
+        $model->size = $file->getSize();
+        $model->created_by = $user->id;
+        $model->updated_by = $user->id;
+
+        $parent->appendNode($model);
+        
+    }
+
+
     public function uploadFilesAction(): Action
     {
         return Action::make('uploadFiles')
@@ -60,8 +99,20 @@ class FilesManager extends Component implements HasActions, HasForms
                     ]),
             ])
             ->action(function (array $data):void {
-                
+                $this->parent_id = $this->currentFolder->id;
+                if($this->parentRules(Auth::user()->id))
+                    {
+                        foreach ($data['files'] as $file) {
+                            /** @var \Illuminate\Http\UploadedFile $file */
+                            $this->saveFile($file, Auth::user(), $this->currentFolder);
+                        }
+                    }
             });
+    }
+
+    public function navigateTo(string $path): void
+    {
+        $this->currentPath = $path;
     }
 
     public function render()
@@ -79,21 +130,6 @@ class FilesManager extends Component implements HasActions, HasForms
         
         $ancestors = $folder->ancestorsAndSelf($folder->id);
 
-        $breadcrumbs = [];
-        foreach ($ancestors as $ancestor) {
-            if (!$ancestor->parent_id) {
-                $breadcrumbs[] = [
-                    'name' => 'My Files',
-                    'path' => route('my-files.index'),
-                ];
-            } else {
-                $breadcrumbs[] = [
-                    'name' => $ancestor->name,
-                    'path' => route('my-files.index', $ancestor->path),
-                ];
-            }
-        }
-
         $files = File::query()
                         ->where('parent_id', $folder->id)
                         ->where('created_by', Auth::user()->id)
@@ -101,7 +137,7 @@ class FilesManager extends Component implements HasActions, HasForms
                         ->orderBy('created_at', 'desc')
                         ->paginate(12);
 
-        return view('livewire.files-manager', compact('files', 'breadcrumbs'));
+        return view('livewire.files-manager', compact('files', 'ancestors'));
     }
 
 }
